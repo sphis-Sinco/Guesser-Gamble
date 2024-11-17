73c3b41964ff16a1d9c8313aa25ffe73bc257327
IdealistCat <mattallan.h@gmail.com>
2024-11-17 17:35:01 -0600

    choice picking game

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,6 +4,19 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var choiceID_right:Int = 0;
+	public var choiceID_wrong:Int = 1;
+
+	public var choices_length:Int = 1;
+
+	override public function new()
+	{
+		if (choices_length < 2)
+			throw '[ERROR] choices_length is an invalid integer (should be 2 or higher)';
+
+		super();
+	}
+
 	override public function create()
 	{
 		super.create();
