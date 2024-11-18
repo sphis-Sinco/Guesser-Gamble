65f2e387ccd2d44bae2ef948f99a7ab72590ac54
IdealistCat <mattallan.h@gmail.com>
2024-11-17 18:51:40 -0600

    fade in

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -1,11 +1,15 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.util.FlxColor;
 
 class MenuState extends FlxState
 {
 	override public function create()
 	{
+		FlxG.camera.fade(FlxColor.BLACK, 0.4, true);
+        
 		super.create();
 	}
 
