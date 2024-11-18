13be01c9d8f7161be4cb35bfa1522ccdecc3c8b8
IdealistCat <mattallan.h@gmail.com>
2024-11-17 19:16:05 -0600

    mainmenu base

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -2,10 +2,14 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.text.FlxText;
 import flixel.util.FlxColor;
 
 class MenuState extends FlxState
 {
+	public var titleText:FlxText = new FlxText("Guesser Gameble", 32);
+	public var coinText:FlxText = new FlxText("Insert 1 coin to start", 16);
+
 	override public function create()
 	{
 		FlxG.camera.fade(FlxColor.BLACK, 0.4, true);
