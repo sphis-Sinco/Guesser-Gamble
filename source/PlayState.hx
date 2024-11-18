efbd8ecd2681f90994a114911ecdf03bb3642ecd
IdealistCat <mattallan.h@gmail.com>
2024-11-17 18:57:06 -0600

    level is static var, refreshChoices function exists

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -12,7 +12,7 @@ class PlayState extends FlxState
 	public var choiceID_wrong:Int = 0;
 	public var choiceID_right:Int = 1;
 
-	public var level:Int = 1;
+	public static var level:Int = 1;
 	public var choices_length:Int = 15;
 
 	public var choices_array:Array<Int> = [];
@@ -51,49 +51,7 @@ class PlayState extends FlxState
 				choices_length = 15;
 		}
 
-		var index:Int = 0;
-		while (index < choices_length)
-		{
-			var random:Float = Math.random();
-			if ((index == (choices_length - 1) || random >= 0.5) && !choices_array.contains(1))
-				choices_array.push(choiceID_right);
-			else
-				choices_array.push(choiceID_wrong);
-
-			index++;
-		}
-
-		trace(choices_array);
-
-		var choiceAm:Int = 0;
-		var choiceAmMAX:Int = 15;
-		var choiceColumn:Int = 0;
-		var padding:Int = 8;
-
-		for (i in 0...choices_array.length)
-		{
-			var choice:FlxSprite = new FlxSprite().makeGraphic(32, 32, FlxColor.RED);
-			choice.ID = choices_array[i];
-
-			if (choiceAm == choiceAmMAX)
-			{
-				choiceColumn++;
-				choiceAm = 0;
-			}
-
-			choice.x = 16 + ((i - (choiceColumn * choiceAmMAX)) * (choice.graphic.width + padding));
-
-			choice.y = FlxG.height - (64 - (choiceColumn * (choice.graphic.height + padding)));
-
-			#if display_answer
-			if (choice.ID == 1)
-				choice.makeGraphic(choice.graphic.width, choice.graphic.height, FlxColor.LIME);
-			#end
-
-			choices.add(choice);
-
-			choiceAm++;
-		}
+		refreshChoices();
 
 		livesText = new FlxText(10, 10);
 		livesText.size = 16;
@@ -164,5 +122,67 @@ class PlayState extends FlxState
 		}
 
 		super.update(elapsed);
+	}
+	public function refreshChoices()
+	{
+		if (choices_array.length > 0)
+		{
+			for (i in 0...choices_array.length)
+			{
+				choices_array.remove(0);
+			}
+		}
+
+		if (choices.length > 0)
+		{
+			for (i in 0...choices.members.length)
+			{
+				choices.members.remove(choices.members[0]);
+			}
+		}
+
+		var index:Int = 0;
+		while (index < choices_length)
+		{
+			var random:Float = Math.random();
+			if ((index == (choices_length - 1) || random >= 0.5) && !choices_array.contains(1))
+				choices_array.push(choiceID_right);
+			else
+				choices_array.push(choiceID_wrong);
+
+			index++;
+		}
+
+		trace(choices_array);
+
+		var choiceAm:Int = 0;
+		var choiceAmMAX:Int = 15;
+		var choiceColumn:Int = 0;
+		var padding:Int = 8;
+
+		for (i in 0...choices_array.length)
+		{
+			var choice:FlxSprite = new FlxSprite().makeGraphic(32, 32, FlxColor.RED);
+			choice.ID = choices_array[i];
+
+			if (choiceAm == choiceAmMAX)
+			{
+				choiceColumn++;
+				choiceAm = 0;
+			}
+
+			choice.x = 16 + ((i - (choiceColumn * choiceAmMAX)) * (choice.graphic.width + padding));
+
+			choice.y = FlxG.height - (64 - (choiceColumn * (choice.graphic.height + padding)));
+
+			#if display_answer
+			if (choice.ID == 1)
+				choice.makeGraphic(choice.graphic.width, choice.graphic.height, FlxColor.LIME);
+			#end
+
+			choices.add(choice);
+
+			choiceAm++;
+		}
 	}
 }
